6e6d35863454b2a8a88e8eaa1822e945b6b5f8ae
Elijah Insua <tmpvar@gmail.com>
2010-12-23 19:50:06 -0500

    re-working the core of motion.js to be a bit more flexible and share more code between the client and server.

--- test/node.js
+++ test/node.js
@@ -1,57 +1,27 @@
-var Motion = require(__dirname + '/../lib/motion.js').Motion;
+var motion = require(__dirname + '/../lib/motion.js').motion,
     vows   = require('vows'),
     assert = require('assert');
 
-vows.describe('Data model').addBatch({
-  'When creating a new Motion object and setting it\'s interval' : {
+vows.describe('Motion.js').addBatch({
+  // Motion Sanity
+  'Sanity' : {
     topic : function() {
-      var motion = new Motion(),
-          self = this;
-
-      this.start = (new Date()).getTime(),
-
-      motion.snapshot_timeout = 100;
-      motion.snapshotActions.push(function() {
-        self.callback(null, motion)
-      });
-      motion.start();
+      return motion();
     },
-    'snapshots are taken on an interval' : function(motion) {
-      assert.isTrue((new Date()).getTime() - this.start >= 100);
-      motion.stop();
-    }
-  },
-  'When attempting to set a Motion value' : {
-    topic : function() {
-      var motion = new Motion();
+    "call sites are sane" : function(err, motion) {
+      assert.isFunction(motion.setMode);
+      assert.isNumber(motion.SERVER);
+      assert.isNumber(motion.CLIENT);
 
-      motion.validationSteps.push(function(name, value) {
-        return name !== 'invalid';
-      });
-      return motion;
-    },
-    'validation controls whether or not the field is set' : function(motion) {
-      assert.isFalse(motion.validate('invalid', true));
+      assert.isSame(motion.setMode(motion.SERVER), motion);
 
-      motion.set('valid', 'valid');
-      assert.strictEqual('valid', motion.get('valid'));
 
-      motion.set('invalid', true);
-      assert.strictEqual(null, motion.get('invalid'));
     }
-  },
-  'When changing values' : {
-    topic : function() {
-      var motion = new Motion();
-      motion.set('diff1', true);
-      return motion;
-    },
-    'they should be cached in a diff until the next snapshot' : function(motion) {
-      assert.isTrue(motion.changes_since_last_snapshot.diff1);
-      motion.snapshot();
-      assert.typeOf(motion.changes_since_last_snapshot.diff1, 'undefined');
-    }
-  },
+  }
+}/*,
+// Client Behavior
+{
+
   'When a snapshot is taken' : {
     topic : function() {
       var motion = new Motion(), self = this;
@@ -69,4 +39,12 @@ vows.describe('Data model').addBatch({
     }
   }
 
-}).export(module);
+},
+// Server Behavior
+{
+
+
+
+
+
+}*/).export(module);
