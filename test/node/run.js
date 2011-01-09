6bdead7ead2357467b62fa87e809f232d537077d
Elijah Insua <tmpvar@gmail.com>
2011-01-08 23:13:35 -0500

    test runner supports running a specific suite

--- test/node/run.js
+++ test/node/run.js
@@ -10,7 +10,7 @@ var suites = [
 
 if (process.argv.length > 2) {
   suites.map(function(suite) {
-    if (process.argv[2] === suite) {
+    if (suite.indexOf(process.argv[2]) > -1) {
       require('nodeunit/reporters/default').run([suite]);
     }
   });
